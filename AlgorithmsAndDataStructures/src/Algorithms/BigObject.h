a9ac41275d54bc39ce9e2be6cb5b14b016ba054e
VilimRoller <vilim.roller@gmail.com>
2021-03-15 11:52:44 +0100

    Big object for benchmark

---
+++ AlgorithmsAndDataStructures/src/Algorithms/BigObject.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <array>
+#include <iostream>
+
+constexpr const int number_of_array_elems = 99;
+
+template<class data_type>
+class BigObject {
+
+public:
+	//TODO add default move constructors
+
+	BigObject() {
+		data = static_cast<data_type>(0);
+	}
+
+	BigObject(data_type input_data) {
+		data = input_data;
+		unused_data_.fill(input_data);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const BigObject& object) {
+		os << object.data;
+		return os;
+	}
+
+	friend bool operator> (const BigObject& left, const BigObject& right) {
+		return left.data > right.data;
+	}
+
+	friend bool operator>= (const BigObject& left, const BigObject& right) {
+		return left.data >= right.data;
+	}
+
+	friend bool operator< (const BigObject& left, const BigObject& right) {
+		return left.data < right.data;
+	}
+
+	friend bool operator<= (const BigObject& left, const BigObject& right) {
+		return left.data <= right.data;
+	}
+
+	friend bool operator== (const BigObject& left, const BigObject& right) {
+		return left.data == right.data;
+	}
+
+	friend bool operator!= (const BigObject& left, const BigObject& right) {
+		return left.data != right.data;
+	}
+
+
+	data_type data;
+
+private:
+
+	std::array<data_type, number_of_array_elems> unused_data_;
+
+};
